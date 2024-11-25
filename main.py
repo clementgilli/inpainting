41e3eb35444e5f134fafc80c41c8e22d3b246a3f
cgilli <cgilli@desktop-qljkbis.home>
2024-11-25 21:47:26 +0100

    clean code

--- main.py
+++ main.py
@@ -5,22 +5,24 @@ from utilities import *
 import sys
 
 if len(sys.argv) < 3:
-    print("Usage: python main.py <path_image> <patch_size> <optional:downsampling_factor>")
+    print("Usage: python main.py <path_image> <patch_size> <optional:search_mode> <optional:downsampling_factor>")
     sys.exit(1)
 
-assert sys.argv[2].isdigit(), "The patch size must be an integer\nUsage: python main.py <path_image> <patch_size> <optional:downsampling_factor>"
+assert sys.argv[2].isdigit(), "The patch size must be an integer\nUsage: python main.py <path_image> <patch_size> <optional:search_mode> <optional:downsampling_factor>"
 
-if len(sys.argv) == 4:
+if len(sys.argv) == 5:
     assert sys.argv[3].isdigit(), "The downsampling factor must be an integer\nUsage: python main.py <path_image> <patch_size> <optional:downsampling_factor>"
     re = Resampling(sys.argv[1],int(sys.argv[2]))
     re.one_level(int(sys.argv[3]))
 
 else:
-
+    mode = "Local"
+    if len(sys.argv) == 4:
+        mode = sys.argv[3]
     try :
-        imgp = PatchedImageColor(sys.argv[1],size=int(sys.argv[2]),search_mode="Local")
+        imgp = PatchedImageColor(sys.argv[1],size=int(sys.argv[2]),search_mode=mode)
     except ValueError:
-        imgp = PatchedImage(sys.argv[1],size=int(sys.argv[2]),search_mode="Local")
+        imgp = PatchedImage(sys.argv[1],size=int(sys.argv[2]),search_mode=mode)
 
     print("====Initialisation====")
     imgp.set_masque(leaf_size=max(imgp.width,imgp.height))
